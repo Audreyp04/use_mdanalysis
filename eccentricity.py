1d338b94efb1bbe01cfc5c9664eb1ff5c1cdb492
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-04-01 16:44:15 -0400

    Create eccentricity.py

---
+++ eccentricity.py
@@ -0,0 +1,32 @@
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis import align
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+
+u=mda.Universe("topology.pdb", "trajectory.xtc")
+ag=u.select_atoms("protein")
+
+def calculate_eccentricity():
+    eccentricity=[]
+    for ts in u.trajectory:
+        I=ag.moment_of_inertia(weights='mass')
+        
+        evals=np.linalg.eigvals(I)
+        evals=np.sort(evals)
+
+        Imin=evals[0]
+        Iavg=np.mean(evals)
+
+        e = 1-(Imin/Iavg)
+        eccentricity.append(e)
+        np.save('eccentricity.npy',eccentricity)
+
+def plot_eccentricity():
+    data=np.load("eccentricity.npy")
+    plt.imshow(data,cmap='viridis')
+    plt.colorbar('Oligomer Eccentricity')
+    plt.xlabel('Time (µs)')
+    plt.ylabel('Eccentricity')
+    plt.yscale(min=0,max=1)
+    plt.savefig()
