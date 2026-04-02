314826e62f0198d6466ec7fb67dc9997d7bc230d
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-04-02 13:52:07 -0400

    Create prot_memb_interactions.py

---
+++ prot_memb_interactions.py
@@ -0,0 +1,73 @@
+import MDAnalysis as mda 
+from MDAnalysis.analysis import distances
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+f=mda.Universe('production.tpr', '../../traj/cat_pbc.xtc')
+
+mem_ca = f.select_atoms('name C1 and resname *PG')
+pro_ca = f.select_atoms('name CA and not resname *PG')
+
+n_mem=len(mem_ca)
+n_pro=len(pro_ca)
+
+print(n_mem)
+print(n_pro)
+
+cutoff = 10.0
+
+# initialize contact accumulator
+contact_sum = np.zeros((n_mem, n_pro))
+
+n_frames = 0
+
+for ts in f.trajectory:
+    dist_arr = distances.distance_array(
+        mem_ca.positions,
+        pro_ca.positions,
+        box=f.dimensions
+    )
+    
+    contacts = (dist_arr <= cutoff).astype(int)
+    contact_sum += contacts
+    n_frames += 1
+
+# average contact frequency
+contact_avg = contact_sum / n_frames
+
+contact_avg[contact_avg < 0.1] = np.nan
+
+cutoff_display = 0.80  # e.g. at least 80% of frames
+
+# find rows/columns that have any interaction
+mem_mask = np.any(contact_avg > cutoff_display, axis=1)
+pro_mask = np.any(contact_avg > cutoff_display, axis=0)
+
+# reduce the matrix
+contact_reduced = contact_avg[mem_mask][:, pro_mask]
+
+# also reduce labels
+mem_resids_reduced = mem_ca.resids[mem_mask]
+pro_resids_reduced = pro_ca.resids[pro_mask]
+
+fig, ax = plt.subplots(figsize=(15,10))
+
+im = ax.imshow(contact_reduced, cmap='viridis', vmin=0, vmax=1)
+
+tick_interval = 1  # now you can show all since it's smaller
+ax.set_yticks(np.arange(len(mem_resids_reduced)))
+ax.set_xticks(np.arange(len(pro_resids_reduced)))
+
+ax.set_yticklabels(mem_resids_reduced)
+ax.set_xticklabels(pro_resids_reduced, rotation=45, ha='right')
+
+plt.ylabel('Membrane residues')
+plt.xlabel('Protein residues')
+plt.title('Trajectory-averaged contacts')
+
+cbar = fig.colorbar(im, fraction=0.025, pad=0.04)
+cbar.ax.set_ylabel('Contact frequency')
+
+plt.savefig('reduced_contact_map.png', bbox_inches='tight')
+plt.close()
