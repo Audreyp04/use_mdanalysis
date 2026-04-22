ccf18b98b8f2da52299a2e11164c6806955ed277
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-04-22 17:26:37 -0400

    Create totalenergy.py

---
+++ totalenergy.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pyedr
+
+#First must have a concatenated EDR file 
+#gmx eneconv -f *.edr -o combined.edr 
+
+path = 'combined.edr'
+dic = pyedr.edr_to_dict(path)
+
+print(dic)
+
+time=dic['Time']/1000
+potential=dic['Total Energy']
+
+plt.plot(time,potential, c='orchid',ls='-')
+plt.xlim(0,2000)
+plt.xlabel('Time (ns)')
+plt.ylabel('Total Energy (kJ/mol)')
+plt.savefig('energy.png')
+plt.close()
