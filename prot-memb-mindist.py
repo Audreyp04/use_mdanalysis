7f7287a16365c52c08654b265ed90a8fa9ff1250
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-04-23 17:51:21 -0400

    Update prot-memb-mindist.py

--- prot-memb-mindist.py
+++ prot-memb-mindist.py
@@ -4,15 +4,12 @@
 import numpy as np
 import MDAnalysis as mda
 import matplotlib.pyplot as plt
-from collections import defaultdict
-from MDAnalysis.lib.distances import distance_array
-
 
 #USER INPUTS
 in_traj = '../traj/cat_pbc_nowat.xtc'
 in_top = 'nowat.tpr'
-outfile = 'hex1_mindist.png'
-title = 'Protein to Membrane Minimum Distances (Hexamer, Rep1)'
+outfile = 'hex1_mid_dist.png'
+title = 'Protein to Membrane Midplane Distances (Hexamer, Rep1)'
 
 #This must be a matplotlib set color OR a HEX Code formated this way: '#ABC123'
 color = 'darkslateblue'
@@ -20,36 +17,32 @@ color = 'darkslateblue'
 #prep for analysis
 def prep():
     u = mda.Universe(in_top, in_traj)
-    prot = u.select_atoms("protein")
-    memb = u.select_atoms("not protein and not resname CL and not resname K and not resname POPX")
+    prot = u.select_atoms("protein and name CA")
+    memb = u.select_atoms("not protein and (name P N C2) and not (resname CL K POPX)")
 
     return u, prot, memb
 
 #calculate contacts
 def calculate_distances(u, prot, memb):
 
-    nframes = 0
-    mindist = []
+    distances = []
+    times=[]
+
     for ts in u.trajectory:
-        nframes += 1
+        z_mid = memb.positions[:, 2].mean() #membrane midplane
+        z_prot = prot.center_of_mass()[2] #prot center of mass 
+        distances.append(abs(z_prot - z_mid) / 10.0)  # nm
+        times.append(ts.time / 1000) #convert to ns 
 
-        dists = distance_array(
-            prot.positions,
-            memb.positions
-        )
-        mindist.append(np.min(dists))
-
-    return mindist, nframes
+    return np.array(times), np.array(distances)
 
 #plot contacts
-def plot_moiety_contacts(mindist, nframes):
+def plot_moiety_contacts(time, dist):
 
-    time = nframes / 10
-
-    plt.plot(time, mindist, c=color)
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize = (8, 8))
+    plt.plot(time, dist, c=color)
     plt.xlabel("Time (ns)")
-    plt.ylabel("Minimum Distance (nm)")
+    plt.ylabel("Distance to Membrane Midplane(nm)")
     plt.title(title)
     plt.tight_layout()
     plt.savefig(outfile, dpi=300, bbox_inches="tight")
@@ -57,5 +50,5 @@ def plot_moiety_contacts(mindist, nframes):
 
 if __name__ == "__main__":
     u, prot, memb = prep()
-    mindist = calculate_distances(u, prot, memb)
-    plot_moiety_contacts(mindist)
+    time, dist = calculate_distances(u, prot, memb)
+    plot_moiety_contacts(time, dist)
